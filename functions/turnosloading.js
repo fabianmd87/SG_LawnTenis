7c58f4f64258291cdcd5522d16eb800c17160407
fabianmd87 <fabianmd87@gmail.com>
2021-07-31 12:58:59 -0300

    modificaciones de sabado

--- functions/turnosloading.js
+++ functions/turnosloading.js
@@ -1,62 +1,53 @@
-class Turno{
-  constructor(turno,cancha,fecha){
-    this.turno=turno;
-    this.cancha=cancha;
-    this.fecha=fecha;
-    }
+class Turno {
+  constructor(turno, cancha, fecha) {
+    this.turno = turno;
+    this.cancha = cancha;
+    this.fecha = fecha;
+  }
 }
 
-class Metodo{
-  agregarTurno(turnos){
-    const listaTurno= document.getElementById('listaTurno');
-    const elemento=document.createElement('div');
-    elemento.innerHTML=`
+class Metodo {
+  agregarTurno(turno) {
+    const listaTurno = document.getElementById("listaTurno");
+    const elemento = document.createElement("div");
+    elemento.innerHTML = `
       <div class="card text-center mt-5">
         <div class="card-body d-flex justify-content-around">
-          <strong>Turno: ${turnos.turno}</strong>
-          <strong>Cancha: ${turnos.cancha}</strong>
-          <strong>Fecha: ${turnos.fecha}</strong>
+          <strong>Turno: ${turno.turno}</strong>
+          <strong>Cancha: ${turno.cancha}</strong>
+          <strong>Fecha: ${turno.fecha}</strong>
         </div>
       </div>  
     `;
     console.log(turno.turno);
     listaTurno.appendChild(elemento); //insertar el elemento en una lista con los turnos
   }
-  
-  resetearFormulario(){
-    document.getElementById('formularioTurno').reset();
+
+  resetearFormulario() {
+    document.getElementById("formularioTurno").reset();
   }
 }
 
-var archivo = document.getElementById('formularioTurno')
+var archivo = document.getElementById("formularioTurno");
 
 if (archivo) {
-  
-  archivo.addEventListener('submit', function(e){
+  archivo.addEventListener("submit", function (e) {
     //Tratar de tomar los valores de los input del formularia
-    const turno = document.getElementById('turno').value;
-    const cancha= document.querySelector('#cancha').value;
-    const fecha=document.getElementById('date').value;
-    
-    console.log(turno)
-    const turnos= new Turno(turno,cancha,fecha);
-  
-    const usarMetodo=  new Metodo;
-  
+    const turno = document.getElementById("turno").value;
+    const cancha = document.querySelector("#cancha").value;
+    const fecha = document.getElementById("date").value;
+
+    console.log(turno);
+    const turnos = new Turno(turno, cancha, fecha);
+
+    const usarMetodo = new Metodo();
+
     usarMetodo.agregarTurno(turnos);
-  
+
     usarMetodo.resetearFormulario();
     e.preventDefault();
   });
 }
-  
-
-
-
-
-
-
-
 
 //const cargaTurnos =(usuarioActual) => {
 //    while (true) {
